28a08ec9786e9ada22d1a818f55a48e5859e2c76
Andrew Stewart <drandrewjstewart@gmail.com>
2020-09-17 13:00:32 +0100

    start script

---
+++ script/script.R
@@ -0,0 +1,12 @@
+library(tidyverse) # Usual tidyverse packages
+
+# We can use the rnorm() function to sample x times from a normal distribution where we specify
+# the mean and the sd.  
+
+rnorm(5, 0, 1)
+rnorm(5, 0, 1)
+
+set.seed(1234)
+rnorm(5, 0, 1)
+set.seed(1234)
+rnorm(5, 0, 1)
